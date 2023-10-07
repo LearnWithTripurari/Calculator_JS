7299c1175306fddfd909899f43707072f9ff8536
Tripurari Singh <tripurarisingh1994@gmail.com>
2023-10-07 16:28:56 +0530

    second commit

--- main.js
+++ main.js
@@ -1,15 +1,56 @@
+let display = "";
 
-// (function(){
+const buttons = document.getElementsByTagName("button");
 
-    const buttons = document.getElementsByTagName("button");
+for (const button of buttons) {
+    button.addEventListener("click", buttonHandler)
+}
 
-    for (const button of buttons) {
-        button.addEventListener("click", function (event) {
-            console.log(event.target.innerText)
-        })
+
+function buttonHandler(event) {
+    const text = event.target.innerText;
+
+    if (validateExpression(text) === 0) {
+        return;
     }
 
-    console.log(buttons)
+    if (text === '=') {
+        calculate();
+    } else if (text === 'c') {
+        display = "";
+    } else if (text === '.') {
+        display += text;
+    } else if (text === '←') {
+        display = display.substring(0, display.length - 1)
+    } else {
+        display += text;
+    }
 
+    displayHandler();
+}
 
-// })()
+function displayHandler() {
+    document.querySelector("#display").innerText = display
+}
+
+function calculate() {
+    display = eval(display).toString() || ""
+    displayHandler()
+}
+
+function validateExpression(text) {
+
+    let lastChar = display.charAt(display.length - 1)
+
+    if ((text === '.' && (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/')) || (lastChar === '.' && (text === '+' || text === '-' || text === '*' || text === '/'))) {
+        display = display.substring(0, display.length - 1);
+    } else if ((text === '+' || text === '-' || text === '*' || text === '/') && (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/')) {
+        return 0;
+    } else if (text === '.' && lastChar === '.') {
+        return 0;
+    }
+
+    return 1;
+
+}
+
